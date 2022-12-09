4e9ce9371429726a1a8075759ca68c6e20cd8d43
Prince Johnson <prince.j@inapp.com>
2022-12-09 19:19:05 +0530

    sidebar component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+import Topbar from "./scenes/Global/Topbar";
+import SidebarComponent from "./scenes/Global/Sidebar";
+// import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar/calendar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -24,10 +24,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
+          <SidebarComponent/>
           <main className='content'>
-            <Topbar/>
-            <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Topbar />
+            {/*<Routes>
+               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
@@ -37,8 +38,8 @@ function App() {
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} />
-            </Routes>
+              <Route path="/geography" element={<Geography />} /> 
+            </Routes>*/}
           </main>
         </div>
       </ThemeProvider>
--- src/scenes/Global/Sidebar.jsx
+++ src/scenes/Global/Sidebar.jsx
@@ -1,9 +1,75 @@
-import React from "react";
+import React from 'react';
+import { useState } from "react";
+import { Sidebar, Menu, MenuItem, ProSidebarProvider } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+// import "react-pro-sidebar/dist/css/styles.css";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
-const Sidebar = () =>{
-    return(
-        <div>Sidebar</div>
-    )
+const SidebarComponent = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const [selected, setSelected] = useState("Dashboard");
+
+    return (
+        <React.Fragment>
+            <ProSidebarProvider>
+                <Box sx={{
+                    "& .pro-sidebar-inner": {
+                        background: `${colors.primary[400]} !important`,
+                    },
+                    "& .pro-icon-wrapper": {
+                        backgroundColor: "transparent !important",
+                    },
+                    "& .pro-inner-item": {
+                        padding: "5px 35px 5px 20px !important",
+                    },
+                    "& .pro-inner-item:hover": {
+                        color: "#868dfb !important",
+                    },
+                    "& .pro-menu-item.active": {
+                        color: "#6870fa !important",
+                    },
+                }}>
+                    <Sidebar>
+                        {/* USER */}
+                        {
+                            isCollapsed && (
+                                <Box mb="25px">
+                                    <Box display="flex" justifyContent="center" alignItems="center">
+                                        <img
+                                            alt="profile-user"
+                                            width="100px"
+                                            height="100px"
+                                            src={`../../../assets/user.png`}
+                                            style={{ cursor: "pointer", borderRadius: "50%" }}
+                                        />
+                                    </Box>
+                                    <Box>
+                                        <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "10px 0 0 0" }} >Prince Johnson</Typography>
+                                        <Typography varient="h5" color={colors.greenAccent[500]}>VP Fancy Admin</Typography>
+                                    </Box>
+                                </Box>
+                            )
+                        }
+                    </Sidebar>
+                </Box>
+            </ProSidebarProvider>
+        </React.Fragment>
+    );
 }
 
-export default Sidebar;
+export default SidebarComponent;
